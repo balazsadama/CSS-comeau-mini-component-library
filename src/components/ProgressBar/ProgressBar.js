82de50bcc571d4c9425134afe2181d08bfe650f9
unknown <balazs.adam.a@gmail.com>
2025-05-03 11:56:43 +0300

    ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,62 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    '--padding': '0px',
+    '--bar-height': '8px',
+  },
+  medium: {
+    '--padding': '0px',
+    '--bar-height': '12px',
+  },
+  large: {
+    '--padding': `${4 / 16}rem`,
+    '--bar-height': '16px',
+  },
+};
+
+const StyledProgressBar = styled.progress`
+-webkit-appearance: none;
+appearance: none;
+
+
+/* &::-moz-progress-bar {
+  background: ${COLORS.primary};
+} */
+
+
+&::-webkit-progress-bar {
+  background: transparent;
+}
+
+&::-webkit-progress-value {
+  background: ${COLORS.primary};
+}
+`;
+
+const ProgressBarContainer = styled.div`
+  padding: var(--padding);
+  border-radius: 8px;
+  background: ${COLORS.transparentGray15};
+  width: fit-content;
+  box-shadow: 0px 2px 4px 0px ${COLORS.transparentGray35} inset;
+`;
+
+const ProgressBarInnerWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+  height: var(--bar-height);
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <ProgressBarContainer style={SIZES[size]}>
+      <ProgressBarInnerWrapper>
+        <StyledProgressBar value={value} max={100}>{value}</StyledProgressBar>
+      </ProgressBarInnerWrapper>
+    </ProgressBarContainer>
+  );
 };
 
 export default ProgressBar;
